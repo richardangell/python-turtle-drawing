73200df7c6906ff46918217cb792b1c78aa88985
Richard Angell <richardangell37@gmail.com>
2024-02-23 20:24:08 +0000

    add command line option to run turtle without updating after every move

--- python_turtle_art/main.py
+++ python_turtle_art/main.py
@@ -1,10 +1,21 @@
 from turtle import Screen, Turtle, Vec2D
+import turtle
+import argparse
 
 from line import draw_curved_line, OffsetFromLine
 
 
 if __name__ == "__main__":
-    turtle = Turtle()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-q", "--quick", action="store_true", help="render image quickly"
+    )
+    args = parser.parse_args()
+
+    if args.quick:
+        turtle.tracer(0, 0)
+
+    turtle_ = Turtle()
 
     s1 = Vec2D(0, 0)
     s2 = Vec2D(-100, 100)
@@ -12,14 +23,17 @@ if __name__ == "__main__":
     s4 = Vec2D(100, 100)
     s5 = Vec2D(0, 0)
 
-    turtle.begin_fill()
+    turtle_.begin_fill()
 
     draw_curved_line(s1, s2, OffsetFromLine(offset=-20), steps=20)
     draw_curved_line(s2, s3, OffsetFromLine(offset=20), steps=20)
     draw_curved_line(s3, s4, OffsetFromLine(offset=20), steps=20)
     draw_curved_line(s4, s1, OffsetFromLine(offset=-20), steps=20)
 
-    turtle.end_fill()
+    turtle_.end_fill()
+
+    if args.quick:
+        turtle.update()
 
     screen = Screen()
     screen.exitonclick()
