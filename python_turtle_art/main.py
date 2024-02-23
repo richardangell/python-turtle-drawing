1f19d6974eb5039c5d7dc4519589be42712da281
Richard Angell <richardangell37@gmail.com>
2024-02-23 20:56:52 +0000

    remove mix of object orientated and procedural turtle use

--- python_turtle_art/main.py
+++ python_turtle_art/main.py
@@ -1,4 +1,4 @@
-from turtle import Screen, Turtle, Vec2D
+from turtle import Screen, Vec2D
 import turtle
 import argparse
 
@@ -15,22 +15,20 @@ if __name__ == "__main__":
     if args.quick:
         turtle.tracer(0, 0)
 
-    turtle_ = Turtle()
-
     s1 = Vec2D(0, 0)
     s2 = Vec2D(-100, 100)
     s3 = Vec2D(0, 200)
     s4 = Vec2D(100, 100)
     s5 = Vec2D(0, 0)
 
-    turtle_.begin_fill()
+    turtle.begin_fill()
 
     draw_curved_line(s1, s2, OffsetFromLine(offset=-20), steps=20)
     draw_curved_line(s2, s3, OffsetFromLine(offset=20), steps=20)
     draw_curved_line(s3, s4, OffsetFromLine(offset=20), steps=20)
     draw_curved_line(s4, s1, OffsetFromLine(offset=-20), steps=20)
 
-    turtle_.end_fill()
+    turtle.end_fill()
 
     if args.quick:
         turtle.update()
