cbc23203db3656554e6a46e441fd91a35616f0ab
Richard Angell <richardangell37@gmail.com>
2024-02-24 10:13:42 +0000

    refactor some functions out into helpers

--- python_turtle_art/helpers.py
+++ python_turtle_art/helpers.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Vec2D
+import turtle as t
 
 
 def jump_to(turtle: Turtle, position: Vec2D) -> None:
@@ -6,3 +7,13 @@ def jump_to(turtle: Turtle, position: Vec2D) -> None:
     turtle.penup()
     turtle.goto(position)
     turtle.pendown()
+
+
+def turn_off_turtle_animation():
+    """Turn off turtle animation."""
+    t.tracer(0, 0)
+
+
+def update_screen():
+    """Update screen (after turning off turtle animation.)"""
+    t.update()
--- python_turtle_art/main.py
+++ python_turtle_art/main.py
@@ -1,19 +1,26 @@
 from turtle import Screen, Vec2D, Turtle
-import turtle as t
 import argparse
 
 import numpy as np
 
 from kite import ConvexKite
 from line import draw_curved_line, OffsetFromLine
+import helpers
 
 
-def aaa():
+def _aaa():
+    """WIP function, overlays kites over curved kite."""
+
+    s1 = Vec2D(0, 0)
+    s2 = Vec2D(-100, 100)
+    s3 = Vec2D(0, 200)
+    s4 = Vec2D(100, 100)
+
     mult = 1
 
     for x in np.linspace(-100, 100, 11):
         for y in np.linspace(-100, 100, 11):
-            ConvexKite(Vec2D(x + (mult * 10), y)).draw(
+            ConvexKite(origin=Vec2D(x + (mult * 10), y), height=30, width=20).draw(
                 turtle_, fill=True, colour="white"
             )
 
@@ -25,23 +32,13 @@ def aaa():
     draw_curved_line(turtle_, s4, s1, OffsetFromLine(offset=20), steps=20)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-q", "--quick", action="store_true", help="render image quickly"
-    )
-    args = parser.parse_args()
-
-    turtle_ = Turtle()
-
-    if args.quick:
-        t.tracer(0, 0)
+def main():
+    """Main drawing function."""
 
     s1 = Vec2D(0, 0)
     s2 = Vec2D(-100, 100)
     s3 = Vec2D(0, 200)
     s4 = Vec2D(100, 100)
-    s5 = Vec2D(0, 0)
 
     turtle_.begin_fill()
 
@@ -56,8 +53,23 @@ if __name__ == "__main__":
         origin=s1, height=100, width=100, diagonal_intersection_along_height=0.8
     ).draw(turtle_, True, "white")
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-q", "--quick", action="store_true", help="render image quickly"
+    )
+    args = parser.parse_args()
+
+    turtle_ = Turtle()
+
     if args.quick:
-        t.update()
+        helpers.turn_off_turtle_animation()
+
+    main()
+
+    if args.quick:
+        helpers.update_screen()
 
     screen = Screen()
     screen.exitonclick()
