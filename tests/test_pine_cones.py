4b250017f08c9d7023c779601fa7943bbf9e8a2a
Richard Angell <richardangell37@gmail.com>
2025-02-08 19:09:23 +0000

    add option to skip test if environment variable disabling beartype is not set

--- tests/test_pine_cones.py
+++ tests/test_pine_cones.py
@@ -1,5 +1,7 @@
+import os
 from pathlib import Path
 
+import pytest
 from PIL import Image, ImageChops
 
 from python_turtle_art import helpers
@@ -8,8 +10,21 @@ from python_turtle_art.pine_cones.main import draw_image
 from python_turtle_art.write import get_canvas_image
 
 
+@pytest.mark.skipif(
+    condition=os.getenv("DISABLE_BEARTYPE") is None,
+    reason="DISABLE_BEARTYPE environment variable not set",
+)
 def test_image_produced():
-    """Check pine_cones.main.draw_image produces the expected image."""
+    """Check pine_cones.main.draw_image produces the expected image.
+
+    Note, the function being tested generates lots of random values so if beartype
+    is active (with the default configuration, which uses random sampling) then the
+    random numbers generated will not be the same as when before beartype was
+    introduced. Beartype can be disabled by setting the DISABLE_BEARTYPE environment
+    variable, which will allow this test to pass. This test is skipped is the
+    DISABLE_BEARTYPE environment variable is not set.
+
+    """
 
     expected_image_file = Path("tests/pine_cones.png")
     expected_image = Image.open(expected_image_file)
