ab073376ce7a2d8650b642711e4fb36e44bc2808
Richard Angell <richardangell37@gmail.com>
2025-02-12 07:16:07 +0000

    add from_origin_and_dimensions method into ConvexKite

--- src/python_turtle_art/polygons/kite/convex_kite.py
+++ src/python_turtle_art/polygons/kite/convex_kite.py
@@ -1,3 +1,6 @@
+from math import sqrt
+from turtle import Vec2D
+
 from ..convex_polygon import ConvexPolygon
 from .kite import Kite
 
@@ -5,4 +8,30 @@ from .kite import Kite
 class ConvexKite(ConvexPolygon, Kite):
     """Class representing a convex kite shape."""
 
-    pass
+    @classmethod
+    def from_origin_and_dimensions(
+        cls,
+        origin: Vec2D,
+        height: int | float = sqrt(20),
+        width: int | float = sqrt(20),
+        diagonal_intersection_along_height: float = 0.5,
+    ) -> Kite:
+        """Define a convex kite from origin point and dimensions.
+
+        Args:
+            origin (Vec2D): bottom point of the kite.
+            height (Union[int, float]): height of the kite.
+            width (Union[int, float]): width of the kite.
+            diagonal_intersection_along_height (float): proportion of the distance
+                along the vertical bisector that the vertical bisector intersects
+                with the horizontal bisector.
+
+        """
+        vertices = cls.calculate_kite_corner_vertices(
+            origin=origin,
+            height=height,
+            width=width,
+            diagonal_intersection_along_height=diagonal_intersection_along_height,
+        )
+
+        return ConvexKite(vertices=vertices)
--- tests/unit/polygons/kite/test_convex_kite.py
+++ tests/unit/polygons/kite/test_convex_kite.py
@@ -14,3 +14,16 @@ def test_cannot_initialise_non_convex_kite():
         ValueError, match="Polygon defined by supplied vertices are not convex."
     ):
         ConvexKite(vertices=vertices)
+
+
+def test_cannot_initialise_non_convex_kite_from_dimensions():
+    """diagonal_intersection_along_height > 1 create arrow head shape."""
+    with pytest.raises(
+        ValueError, match="Polygon defined by supplied vertices are not convex."
+    ):
+        ConvexKite.from_origin_and_dimensions(
+            origin=Vec2D(0, 0),
+            height=50,
+            width=25,
+            diagonal_intersection_along_height=1.1,
+        )
