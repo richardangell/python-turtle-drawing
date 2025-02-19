6a74fc5613e8f5c6bab68c2121cd0f190577225c
Richard Angell <richardangell37@gmail.com>
2025-02-19 20:46:46 +0000

    add HashFill class

--- src/python_turtle_art/filling/__init__.py
+++ src/python_turtle_art/filling/__init__.py
@@ -1,5 +1,6 @@
 from .colour_fill import ColourFill
+from .stripes.hash_fill import HashFill
 from .stripes.horizontal_stripe_fill import HorizontalStipeFill
 from .stripes.vertical_stripe_fill import VerticalStripeFill
 
-__all__ = ["ColourFill", "HorizontalStipeFill", "VerticalStripeFill"]
+__all__ = ["ColourFill", "HashFill", "HorizontalStipeFill", "VerticalStripeFill"]
