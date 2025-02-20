9d9f418ad9fab02929dff2a798431be0fabc84d2
Richard Angell <richardangell37@gmail.com>
2025-02-20 19:23:13 +0000

    add code to draw image

--- src/python_turtle_art/drawings/__init__.py
+++ src/python_turtle_art/drawings/__init__.py
@@ -1,3 +1,4 @@
 from .pine_cones.main import draw_image as draw_image_pine_cones
+from .stars_3bp.main import draw_image as draw_image_stars_3bp
 
-__all__ = ["draw_image_pine_cones"]
+__all__ = ["draw_image_pine_cones", "draw_image_stars_3bp"]
