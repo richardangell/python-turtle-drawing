17fec59946680626f83d5988a1a4b9777bfac315
Richard Angell <richardangell37@gmail.com>
2025-02-28 08:42:44 +0000

    update temp draw file

--- src/python_turtle_art/draw.py
+++ src/python_turtle_art/draw.py
@@ -6,49 +6,55 @@ from python_turtle_art.polygons.kites.convex_kite import ConvexKite
 
 
 def draw_image(turtle: RawTurtle):
-    l = 48
-    u = 48
+    l = 50
+    u = 50
 
-    Line(vertices=(Vec2D(-l, -l), Vec2D(u, -l))).draw(turtle, size=1)
-    Line(vertices=(Vec2D(u, -l), Vec2D(u, u))).draw(turtle, size=1)
-    Line(vertices=(Vec2D(u, u), Vec2D(-l, u))).draw(turtle, size=1)
-    Line(vertices=(Vec2D(-l, u), Vec2D(-l, -l))).draw(turtle, size=1)
+    kite = ConvexKite(vertices=(Vec2D(-l, -l), Vec2D(u, -l), Vec2D(u, u), Vec2D(-l, u)))
+    kite.draw(turtle, size=0.5)
+    kite.fill(turtle, ColourFill("yellow"))
+
+    # Line(vertices=()).draw(turtle, size=0.5)
+    # Line(vertices=(Vec2D(u, -l), Vec2D(u, u))).draw(turtle, size=0.5)
+    # Line(vertices=(Vec2D(u, u), Vec2D(-l, u))).draw(turtle, size=0.5)
+    # Line(vertices=(Vec2D(-l, u), Vec2D(-l, -l))).draw(turtle, size=0.5)
 
     # bottom left
     l = -49
     u = -2
     kite = ConvexKite(vertices=(Vec2D(l, l), Vec2D(u, l), Vec2D(u, u), Vec2D(l, u)))
-    kite.draw(turtle, size=1, colour="grey")
+    kite.draw(turtle, size=0.5, colour="grey")
 
     # top right
     l = 49
     u = 2
     kite = ConvexKite(vertices=(Vec2D(l, l), Vec2D(u, l), Vec2D(u, u), Vec2D(l, u)))
-    kite.draw(turtle, size=1, colour="grey")
+    kite.draw(turtle, size=0.5, colour="grey")
 
     # bottom right
     l = -49
     u = -2
     kite = ConvexKite(vertices=(Vec2D(-l, l), Vec2D(-l, u), Vec2D(-u, u), Vec2D(-u, l)))
-    kite.draw(turtle, size=1, colour="grey")
+    kite.draw(turtle, size=0.5, colour="grey")
 
     # top left
     l = -49
     u = -2
     kite = ConvexKite(vertices=(Vec2D(l, -l), Vec2D(l, -u), Vec2D(u, -u), Vec2D(u, -l)))
-    kite.draw(turtle, size=1, colour="grey")
+    kite.draw(turtle, size=0.5, colour="grey")
 
     yellow_fill = ColourFill("yellow")
     # kite.fill(turtle, yellow_fill)
     # kite.draw(turtle, size=1)
 
-    turtle.penup()
-    turtle.goto(0, 0)
-    turtle.pendown()
-    turtle.dot(size=1)
+    # turtle.penup()
+    # turtle.goto(0, 0)
+    # turtle.pendown()
+    # turtle.dot(size=1)
 
     # Line(vertices=(Vec2D(-100, 0), Vec2D(100, 0))).draw(turtle, size=1)
     # Line(vertices=(Vec2D( 0, 100), Vec2D(0, -100))).draw(turtle, size=1)
 
-    Line(vertices=(Vec2D(-100, -100), Vec2D(100, 100))).draw(turtle, size=1)
-    Line(vertices=(Vec2D(-100, 100), Vec2D(100, -100))).draw(turtle, size=1)
+    Line(vertices=(Vec2D(-40, -30), Vec2D(20, 45))).draw(turtle, size=0.5)
+    # Line(vertices=(Vec2D(-100, 100), Vec2D(100, -100))).draw(turtle, size=0.5)
+
+    # time.sleep(10)
