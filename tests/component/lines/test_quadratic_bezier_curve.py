073f7f8e8115e5be623e2f33042a7edb6ed31307
Richard Angell <richardangell37@gmail.com>
2025-03-02 13:13:37 +0000

    udate QBC tests

--- tests/component/lines/test_quadratic_bezier_curve.py
+++ tests/component/lines/test_quadratic_bezier_curve.py
@@ -11,7 +11,7 @@ from ...helpers import assert_image_difference_within_tolerance
 
 
 @pytest.mark.window_dimensions((100, 100))
-def test_quadratic_bezier_curve(setup_screen_with_squares_background):
+def test_control_point_after_line_end_point(setup_screen_with_squares_background):
     # Arrange
 
     turtle, screen = setup_screen_with_squares_background
@@ -19,54 +19,66 @@ def test_quadratic_bezier_curve(setup_screen_with_squares_background):
     # Act
 
     QuadraticBezierCurve.from_start_and_end(
-        start=Vec2D(-40, 10),
-        end=Vec2D(-40, 40),
-        off_line=OffsetFromLine(0.5, 10),
+        start=Vec2D(30, 40),
+        end=Vec2D(30, 10),
+        off_line=OffsetFromLine(2.5, -100),
+    ).draw(turtle, size=3)
+
+    # Assert
+
+    actual_image = get_canvas_image(screen, 100, 100, True)
+
+    expected_image_file = Path(
+        "tests/component/lines/expected_control_point_after_line_end.png"
+    )
+    expected_image = Image.open(expected_image_file)
+
+    assert_image_difference_within_tolerance(
+        actual=actual_image,
+        expected=expected_image,
+        tolerance_non_matching_pixels=0,
+        tolerance_adjacent_pixels=0,
+    )
+
+
+@pytest.mark.window_dimensions((100, 100))
+def test_increasing_distance_of_control_point_from_line(
+    setup_screen_with_squares_background,
+):
+    # Arrange
+
+    turtle, screen = setup_screen_with_squares_background
+
+    # Act
+    QuadraticBezierCurve.from_start_and_end(
+        start=Vec2D(-30, -30),
+        end=Vec2D(30, 30),
+        off_line=OffsetFromLine(0.3, 10),
     ).draw(turtle, size=1)
 
     QuadraticBezierCurve.from_start_and_end(
-        start=Vec2D(-30, 10),
-        end=Vec2D(-30, 40),
-        off_line=OffsetFromLine(0.5, 10),
-    ).draw(turtle, size=2)
+        start=Vec2D(-30, -30),
+        end=Vec2D(30, 30),
+        off_line=OffsetFromLine(0.3, 20),
+    ).draw(turtle, size=1)
 
     QuadraticBezierCurve.from_start_and_end(
-        start=Vec2D(-5, 10),
-        end=Vec2D(-5, 40),
+        start=Vec2D(-30, -30),
+        end=Vec2D(30, 30),
+        off_line=OffsetFromLine(0.3, 30),
+    ).draw(turtle, size=1)
+
+    QuadraticBezierCurve.from_start_and_end(
+        start=Vec2D(-30, -30),
+        end=Vec2D(30, 30),
         off_line=OffsetFromLine(0.3, 40),
     ).draw(turtle, size=1)
 
     QuadraticBezierCurve.from_start_and_end(
-        start=Vec2D(-40, -40),
-        end=Vec2D(40, 20),
-        off_line=OffsetFromLine(0.8, 35),
-    ).draw(turtle, size=1)
-
-    QuadraticBezierCurve.from_start_and_end(
-        start=Vec2D(10, -10),
-        end=Vec2D(40, -10),
-        off_line=OffsetFromLine(0.8, -10),
-    ).draw(turtle, size=1)
-
-    QuadraticBezierCurve.from_start_and_end(
-        start=Vec2D(10, -30),
-        end=Vec2D(40, -30),
-        off_line=OffsetFromLine(0.2, 10),
-    ).draw(turtle, size=3)
-
-    # control point beyond the line end point
-    QuadraticBezierCurve.from_start_and_end(
-        start=Vec2D(-5, -5),
-        end=Vec2D(-5, -30),
-        off_line=OffsetFromLine(1.5, -25),
-    ).draw(turtle, size=1)
-
-    # zero offset
-    QuadraticBezierCurve.from_start_and_end(
-        start=Vec2D(10, 40),
-        end=Vec2D(40, 40),
-        off_line=OffsetFromLine(1.5, 0),
-    ).draw(turtle, size=1)
+        start=Vec2D(-10, -25),
+        end=Vec2D(30, -25),
+        off_line=OffsetFromLine(0.7, -30),
+    ).draw(turtle, size=2)
 
     # Assert
 
