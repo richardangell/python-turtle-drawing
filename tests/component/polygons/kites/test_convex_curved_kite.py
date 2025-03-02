2406772b70b2b9697d73a1e1b4bc52e3d8c9b78d
Richard Angell <richardangell37@gmail.com>
2025-03-02 14:06:42 +0000

    test for ConvexCurvedKite

--- tests/component/polygons/kites/test_convex_curved_kite.py
+++ tests/component/polygons/kites/test_convex_curved_kite.py
@@ -4,33 +4,40 @@ from turtle import Vec2D
 import pytest
 from PIL import Image
 
-from python_turtle_art.polygons.kites import ConvexKite
+from python_turtle_art.lines import OffsetFromLine
+from python_turtle_art.polygons.kites import ConvexCurvedKite
 from python_turtle_art.write import get_canvas_image
 
 from ....helpers import assert_image_difference_within_tolerance
 
 
 @pytest.mark.window_dimensions((100, 100))
-def test_convex_kite(setup_screen_with_squares_background):
+def test_convex_curved_kite(setup_screen_with_squares_background):
     # Arrange
 
     turtle, screen = setup_screen_with_squares_background
 
     # Act
 
-    ConvexKite.from_origin_and_dimensions(
-        origin=Vec2D(0, -30),
+    ConvexCurvedKite.from_origin_and_dimensions(
+        origin=Vec2D(-10, -40),
         width=50,
-        height=70,
-        diagonal_intersection_along_height=0.7,
-    ).draw(turtle, size=2)
+        height=80,
+        diagonal_intersection_along_height=0.5,
+        off_lines=(
+            OffsetFromLine(0.5, 10),
+            OffsetFromLine(0.8, 10),
+            OffsetFromLine(0.2, 10),
+            OffsetFromLine(0.5, 10),
+        ),
+    ).draw(turtle, size=4)
 
     # Assert
 
     actual_image = get_canvas_image(screen, 100, 100, True)
 
     expected_image_file = Path(
-        "tests/component/polygons/kites/expected_convex_kite.png"
+        "tests/component/polygons/kites/expected_convex_curved_kite.png"
     )
     expected_image = Image.open(expected_image_file)
 
